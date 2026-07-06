171e2317ac7102c91b1cf4a9e472adb0460c730b
keroppi <yejiseo0725@gmail.com>
2026-07-06 14:58:34 +0900

    symbol

--- js/mapset.js
+++ js/mapset.js
@@ -81,3 +81,21 @@ function findInter(set1, set2) {
   return new Set(array);
 }
 console.log(findInter(set1, set2));
+
+// Symbol
+// 유일한 값, 키 생성에 유용
+const map = new Map();
+// const key1 = "key";
+// const key2 = "key";
+
+const key1 = Symbol("key");
+const key2 = Symbol("key");
+map.set(key1, "Hello");
+console.log(map.get(key2));
+console.log(key1 === key2);
+
+// 동일한 이름으로 하나의 키를 사용하고 싶다면, Symbol.for
+const k1 = Symbol.for("key");
+const k2 = Symbol.for("key");
+console.log(k1 === k2);
+// 보안을 위한 키 사용 (map 에서)
