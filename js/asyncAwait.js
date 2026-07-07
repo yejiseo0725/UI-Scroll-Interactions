83fca6b2821760e7ff63777cdd3d1b8cac7a8ecc
keroppi <yejiseo0725@gmail.com>
2026-07-07 18:14:20 +0900

    async and await

---
+++ js/asyncAwait.js
@@ -0,0 +1,43 @@
+// 싱글 쓰레드 Single Thread
+// 하나의 싱글 컨텍스트 스택
+// js 는 기본적으로 '동기적(순서대로)' 으로 진행
+function a() {
+  for (let i = 0; i < 100000; i++);
+  // 이것 때문에 마지막 result 결과가 매우 늦게 나오게 됨.
+  return 1;
+}
+function b() {
+  return a() + 1;
+}
+function c() {
+  return b() + 1;
+}
+
+const result = c();
+console.log(result);
+// 호출 순서가 콜스택에 저장됨
+
+// Wep APIs --> 도움 받아서 비동기적(병렬적)으로 실행되게 함.
+// DOM API, setTimeout, setInterval, fetch, event listener
+function execute() {
+  console.log("1");
+  setTimeout(() => {
+    console.log("2");
+  }, 3000);
+  console.log("3");
+}
+execute();
+
+// quiz
+function runInDelay(callback, seconds) {
+  if (!callback) {
+    throw new Error("callback 함수 전달 해야 함");
+  }
+  if (!seconds || seconds < 0) {
+    throw new Error("0보다 커야 함");
+  }
+  setTimeout(callback, seconds * 1000);
+}
+runInDelay(() => {
+  console.log("어떤 호랑이가 나올까");
+}, 2);
