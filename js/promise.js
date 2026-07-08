2eee7efaf824cefe60f5830ec1d46d08d421b75e
keroppi <yejiseo0725@gmail.com>
2026-07-08 15:54:35 +0900

    promise all, race, error--allsettled

--- js/promise.js
+++ js/promise.js
@@ -29,3 +29,49 @@ getChicken()
   .then(fetchEgg)
   .then(fryEgg)
   .then(console.log);
+
+// 바나나와 사과
+function getBanana() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("bananana");
+    }, 1000);
+  });
+}
+
+function getApple() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("appleee");
+    }, 2000);
+  });
+}
+
+function getOrange() {
+  return Promise.reject(new Error("no orange"));
+}
+
+// 바나나와 사과를 같이 가지고 오기
+getBanana() //
+  .then((banana) =>
+    getApple() //
+      .then((apple) => [banana, apple])
+  )
+  .then(console.log);
+
+// all: 병렬적으로 한번에 모든 Promise 들을 실행!
+Promise.all([getBanana(), getApple()]) //
+  .then((fruits) => console.log("all", fruits));
+
+// race: 주어진 promise 중에 제일 빨리 수행된 것이 이김!
+Promise.race([getBanana(), getApple()]) //
+  .then((fruit) => console.log("race", fruit));
+
+// error
+Promise.all([getBanana(), getApple(), getOrange()]) //
+  .then((fruits) => console.log("all-error", fruits))
+  .catch(console.log);
+
+Promise.allSettled([getBanana(), getApple(), getOrange()]) //
+  .then((fruits) => console.log("all-settle", fruits))
+  .catch(console.log);
